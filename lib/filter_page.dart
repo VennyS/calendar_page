a823b926eac78040f9ceb5c242b3eeb7398ada43
Danil Panarin <panarin.danil.08@gmail.com>
2024-08-22 23:04:09 +0500

    Страница с будущими фильтрами

---
+++ lib/filter_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:widgets/custom_button.dart';
+
+class FilterPage extends StatelessWidget {
+  const FilterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Фильтры",
+            style: GoogleFonts.inter(fontWeight: FontWeight.w800, fontSize: 18),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: SvgPicture.asset(
+                "assets/svgs/cross.svg",
+                // TODO: Поправить цвет иконки.
+                colorFilter:
+                    const ColorFilter.mode(Colors.black, BlendMode.srcIn),
+              )),
+          actions: [
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Очистить",
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                    color: const Color(0xFFA03FFF),
+                  ),
+                  textAlign: TextAlign.right,
+                ))
+          ],
+        ),
+        body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: CustomButtonWidget(
+                  text: "Применить",
+                  accentColor: const Color(0xFFA03FFF),
+                  variant: CustomButtonVariants.primary,
+                  onPressed: () => Navigator.pop(context)),
+            )));
+  }
+}
